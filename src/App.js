d30b431369055a9f0eb5894cb110e8b923fe73cd
Harshit Jindal <web.harshit799@gmail.com>
2021-03-01 23:51:54 +0530

    nothing Much

--- src/App.js
+++ src/App.js
@@ -1,53 +1,35 @@
-import React from 'react'
-import { Switch, Route } from "react-router-dom";
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import React from "react";
+import {Switch, Route} from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.page";
 import SignInPage from "./pages/signinpage/signinpage.component";
 import Header from "./components/header/header.component";
-// class App extends React.Component {
-//   constructor() {
-//     super();
 
-//     this.state = {
-//       string: 'Hello'
-//     }
-//   }
-//   // componentDidMount() {
-
-//   // }
-//   // componentWillUnmount() {
-
-//   // }
-//   render() {
-//     return (
-//       <h1>
-//         <HomePage />
-
-//       </h1>
-
-//     )
-//   }
-
-// }
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+function HatsPage() {
+    return (
+        <div>
+            <h1>HATS PAGE
+            </h1>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/signin' component={SignInPage} />
-        <Route path='/hats' component={HatsPage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
-    </div>
-  )
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path="/"
+                    component={HomePage}/>
+                <Route exact path="/signin"
+                    component={SignInPage}/>
+                <Route path="/hats"
+                    component={HatsPage}/>
+                <Route path="/shop"
+                    component={ShopPage}/>
+            </Switch>
+        </div>
+    );
 }
 export default App;
